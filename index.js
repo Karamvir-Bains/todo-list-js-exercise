0ff8a121b70a76855699031ab01296101a78f769
Karamvir-Bains <kbains2000@gmail.com>
2022-10-09 11:05:42 -0600

    Lintned the file

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-// Arrays to keep track of each task's state
-const taskTitles = [];
-const taskComplete = [];
-const taskDescriptions = [];
-
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
-function newTask(title, description) {
+const newTask = function(title, description) {
   const task = {
     title: title,
-    description, description,
+    description: description,
     complete: false,
     logState: function() {
       console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
@@ -18,7 +13,7 @@ function newTask(title, description) {
     }
   };
   return task;
-}
+};
 
 // DRIVER CODE CHANGES BELOW
 
@@ -29,3 +24,5 @@ const tasks = [task1, task2];
 task1.logState(); // Clean Cat Litter has not been completed
 task1.markCompleted();
 task1.logState(); // Clean Cat Litter has been completed
+
+console.log(tasks);
